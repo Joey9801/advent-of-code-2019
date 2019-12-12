144ef19ea78417b733477bd3117fd84e612e9e72
Joe Roberts <joe@jwjr.co.uk>
2019-12-12 21:43:12 +0000

    Day 1, part 2 solution

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -2,7 +2,18 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn fuel_required(mass: u64) -> u64 {
-    (mass / 3) - 2
+    std::cmp::max(mass / 3, 2) - 2
+}
+
+fn fuel_required_recursive(mass: u64) -> u64 {
+    let mut total = 0;
+    let mut extra = fuel_required(mass);
+    while extra > 0 {
+        total += extra;
+        extra = fuel_required(extra);
+    }
+
+    total
 }
 
 fn main() -> io::Result<()> {
@@ -12,7 +23,7 @@ fn main() -> io::Result<()> {
     let sum: u64 = reader.lines()
         .map(|l| l.expect("Failed to read a line"))
         .map(|l| l.trim().parse::<u64>().expect(&format!("{} wasn't a valid u64", l)))
-        .map(fuel_required)
+        .map(fuel_required_recursive)
         .sum();
 
     println!("Sum of fuel required: {}", sum);
