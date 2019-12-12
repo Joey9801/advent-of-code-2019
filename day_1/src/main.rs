40d5d27dda55a84d6788f93c1cbe0137fd6b0138
Joe Roberts <joe@jwjr.co.uk>
2019-12-12 21:34:41 +0000

    Day 1, part 1 solution

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn fuel_required(mass: u64) -> u64 {
+    (mass / 3) - 2
+}
+
+fn main() -> io::Result<()> {
+    let file = File::open("./input.txt")?;
+    let reader = BufReader::new(file);
+
+    let sum: u64 = reader.lines()
+        .map(|l| l.expect("Failed to read a line"))
+        .map(|l| l.trim().parse::<u64>().expect(&format!("{} wasn't a valid u64", l)))
+        .map(fuel_required)
+        .sum();
+
+    println!("Sum of fuel required: {}", sum);
+
+    Ok(())
 }
