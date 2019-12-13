5a8f1384aec4d3ae52bf4bd583df701a22d80397
Joe Roberts <joe@jwjr.co.uk>
2019-12-13 22:28:16 +0000

    Day 4, part 1 solution

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,21 @@
+fn adjacency_req(candidate: &str) -> bool {
+    candidate.as_bytes()
+        .windows(2)
+        .any(|pair| pair[0] == pair[1])
+}
+
+fn ascending_req(candidate: &str) -> bool {
+    candidate.as_bytes()
+        .windows(2)
+        .all(|pair| pair[0] <= pair[1])
+}
+
 fn main() {
-    println!("Hello, world!");
+    let count = (372304..847061)
+        .map(|x| format!("{}", x))
+        .filter(|x| adjacency_req(&x))
+        .filter(|x| ascending_req(&x))
+        .count();
+
+    println!("There were {} valid candidate passwords", count);
 }
