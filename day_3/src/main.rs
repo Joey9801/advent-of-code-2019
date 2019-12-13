a1670b79c451efeccc55c5d105865a5b71e2cf3f
Joe Roberts <joe@jwjr.co.uk>
2019-12-13 18:39:06 +0000

    Day 3, part 1 solution

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,3 +1,164 @@
-fn main() {
-    println!("Hello, world!");
+//! Define the coordinate system to be one where (1, 1) is a vector pointing up and right.
+
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+
+#[derive(Clone, Copy, Debug)]
+struct Vector2 {
+    x: i64,
+    y: i64,
+}
+
+impl Vector2 {
+    fn new(x: i64, y: i64) -> Self {
+        Self {
+            x,
+            y
+        }
+    }
+
+    fn l1_norm(&self) -> u64 {
+        self.x.abs() as u64 + self.y.abs() as u64
+    }
+}
+
+impl std::ops::Sub for Vector2 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl std::ops::AddAssign for Vector2 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Line {
+    /// The starting coordinate of this line
+    origin: Vector2,
+
+    /// The vector from the start of this wire to its terminus
+    /// 
+    /// Expect that this is zero in precisely one of (x, y)
+    span: Vector2,
+}
+
+impl Line {
+    fn contains_point(&self, point: Vector2) -> bool {
+        let in_x = match self.span.x.signum() {
+            0 => point.x == self.origin.x,
+            1 => point.x > self.origin.x && point.x < (self.origin.x + self.span.x),
+            -1 => point.x < self.origin.x && point.x > (self.origin.x + self.span.x),
+            _ => unreachable!(),
+        };
+
+        let in_y = match self.span.y.signum() {
+            0 => point.y == self.origin.y,
+            1 => point.y > self.origin.y && point.y < (self.origin.y + self.span.y),
+            -1 => point.y < self.origin.y && point.y > (self.origin.y + self.span.y),
+            _ => unreachable!(),
+        };
+
+        in_x && in_y
+    }
+
+    /// If this line intersects with the other, the point at which they intersect
+    fn intersection_point(&self, other: &Line) -> Option<Vector2> {
+        if self.span.x == 0 && other.span.x == 0 {
+            // The lines are parallel -> they don't intersect
+            return None;
+        }
+
+        // The point where the lines would intersect if they were infinitely long
+        // Only valid because each line is axis aligned.
+        let point = Vector2 {
+            x: self.origin.x * self.span.y.abs().signum() + other.origin.x * other.span.y.abs().signum(),
+            y: self.origin.y * self.span.x.abs().signum() + other.origin.y * other.span.x.abs().signum(),
+        };
+
+        if self.contains_point(point) && other.contains_point(point) {
+            Some(point)
+        } else {
+            None
+        }
+    }
+}
+
+/// Represents a wire made up multiple line segments
+struct Wire {
+    /// The line segments in this wire go between the nodes.
+    /// 
+    /// For AoC day 2, part 1, the first node should be (0, 0)
+    nodes: Vec<Vector2>,
+}
+
+impl Wire {
+    fn from_puzzle_input(input: &str) -> Self {
+        assert!(input.is_ascii());
+
+        let mut cursor = Vector2::new(0, 0);
+        let mut nodes = vec![cursor.clone()];
+
+        for instr in input.split(",") {
+            let dir = &instr[0..1];
+            let len: i64  = instr[1..]
+                .parse()
+                .expect(&format!("\"{}\" wasn't a valid instruction", instr));
+
+            match dir {
+                "U" => cursor += Vector2::new(0, len),
+                "D" => cursor += Vector2::new(0, -len),
+                "L" => cursor += Vector2::new(-len, 0),
+                "R" => cursor += Vector2::new(len, 0),
+                other => panic!(format!("Unknown direction '{}'", other)),
+            }
+
+            nodes.push(cursor.clone());
+        }
+
+        Self {
+            nodes
+        }
+    }
+
+    fn iter_lines<'a>(&'a self) -> impl Iterator<Item = Line> + 'a {
+        self.nodes
+            .windows(2)
+            .map(|parts| Line {
+                origin: parts[0],
+                span: parts[1] - parts[0],
+            })
+    }
+}
+
+fn main()  {
+    let file = File::open("./input.txt").expect("Failed to open input.txt");
+    let reader = BufReader::new(file);
+
+    let mut wires = reader.lines()
+        .map(|l| l.expect("Failed to read line"))
+        .map(|l| Wire::from_puzzle_input(&l.trim()));
+
+    let a = wires.next().expect("Expected exactly two wires");
+    let b = wires.next().expect("Expected exactly two wires");
+
+    let min_intersection = a.iter_lines().filter_map(|a_line| {
+        b.iter_lines().filter_map(|b_line| {
+            a_line.intersection_point(&b_line).map(|i| i.l1_norm())
+        })
+        .min()
+    }).min();
+
+    println!("Minimum intersection: {:?}", min_intersection);
 }
