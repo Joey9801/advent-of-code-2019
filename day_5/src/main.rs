cd6233258bfb1f86ad6af850daf9ae344cde0250
Joe Roberts <joe@jwjr.co.uk>
2019-12-14 22:23:18 +0000

    Day 5: refactor intcode vm

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,80 +1,149 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-const INSTR_ADD: usize = 1;
-const INSTR_MUL: usize = 2;
-const INSTR_TERM: usize = 99;
+type ProgramElement = isize;
 
-trait Offset {
-    fn offset(&self, offset: isize) -> Self;
+enum ParameterMode {
+    Position,
+    Immediate,
 }
 
-impl Offset for usize {
-    fn offset(&self, offset: isize) -> usize {
-        if offset > 0 {
-            self + (offset as usize)
-        } else {
-            self - (-offset as usize)
+impl From<u8> for ParameterMode {
+    fn from(code: u8) -> Self {
+        match code {
+            0 => ParameterMode::Position,
+            1 => ParameterMode::Immediate,
+            code => panic!("Unrecognized parameter mode code: {}", code)
         }
     }
 }
 
+struct Parameter {
+    mode: ParameterMode,
+    contents: ProgramElement,
+}
+
+impl Parameter {
+    fn read(&self, state: &ProgramState) -> ProgramElement {
+        match self.mode {
+            ParameterMode::Position => state.values[self.contents as usize],
+            ParameterMode::Immediate => self.contents,
+        }
+    }
+
+    fn write(&self, state: &mut ProgramState, value: ProgramElement) {
+        match self.mode {
+            ParameterMode::Position => state.values[self.contents as usize] = value,
+            ParameterMode::Immediate => panic!("Attempting to write to an immediate mode parameter"),
+        }
+    }
+}
+
+enum OpCode {
+    Add,
+    Multiply,
+    Terminate,
+}
+
+impl OpCode {
+    fn from_element(element: &ProgramElement) -> Self {
+        match element % 100 {
+            1 => OpCode::Add,
+            2 => OpCode::Multiply,
+            99 => OpCode::Terminate,
+            code => panic!("Unrecognized opcode: {}", code)
+        }
+    }
+
+    fn length(&self) -> usize {
+        match self {
+            OpCode::Add => 4,
+            OpCode::Multiply => 4,
+            OpCode::Terminate => 1,
+        }
+    }
+}
+
+
+struct Instruction {
+    opcode: OpCode,
+    parameters: [Option<Parameter>; 4]
+}
+
+impl Instruction {
+    fn fetch_and_decode(state: &ProgramState) -> Self {
+        let opcode = OpCode::from_element(&state.values[state.program_counter]);
+
+        let mut parameters = [None, None, None, None];
+        let mut parameter_modes = state.values[state.program_counter] / 100;
+
+        for i in 1..opcode.length() {
+            let mode = ((parameter_modes % 10) as u8).into();
+            parameter_modes /= 10;
+            let contents = state.values[state.program_counter + i].clone();
+            parameters[i - 1] = Some(Parameter {
+                mode,
+                contents,
+            });
+        }
+
+        Self {
+            opcode,
+            parameters,
+        }
+    }
+
+    fn read_param(&self, idx: usize, state: &ProgramState) -> ProgramElement {
+        self.parameters[idx].as_ref().unwrap().read(state)
+    }
+
+    fn write_param(&self, idx: usize, state: &mut ProgramState, value: ProgramElement) {
+        self.parameters[idx].as_ref().unwrap().write(state, value)
+    }
+
+    fn execute(&self, state: &mut ProgramState) {
+        match self.opcode {
+            OpCode::Add => {
+                let a = self.read_param(0, state);
+                let b = self.read_param(1, state);
+                self.write_param(2, state, a + b);
+            }
+            OpCode::Multiply => {
+                let a = self.read_param(0, state);
+                let b = self.read_param(1, state);
+                self.write_param(2, state, a * b);
+            }
+            OpCode::Terminate => state.terminated = true,
+        }
+    }
+}
+
+
 struct ProgramState {
-    values: Vec<usize>,
+    values: Vec<ProgramElement>,
     program_counter: usize,
+    terminated: bool,
 }
 
 impl ProgramState {
-    fn new(values: Vec<usize>) -> Self {
+    fn new(values: Vec<ProgramElement>) -> Self {
         debug_assert!(values.len() > 0);
 
         Self {
             values,
             program_counter: 0,
+            terminated: false,
         }
-    }
-
-    /// Returns the value at a given offset from the program counter
-    fn read_rel(&self, offset: isize) -> usize {
-        self.values[self.program_counter.offset(offset)]
-    }
-
-    /// Returns the value at the address stored at the given offset from the program counter
-    fn read_rel_ptr(&self, offset: isize) -> usize {
-        let idx = self.read_rel(offset);
-        self.values[idx]
-    }
-
-    fn terminated(&self) -> bool {
-        self.read_rel(0) == INSTR_TERM
     }
 
     fn progress_state(&mut self) {
-        if self.terminated() {
-            return;
-        }
-
-        match self.read_rel(0) {
-            INSTR_ADD => {
-                let a = self.read_rel_ptr(1);
-                let b = self.read_rel_ptr(2);
-                let c_idx = self.read_rel(3);
-                self.values[c_idx] = a + b;
-            },
-            INSTR_MUL => {
-                let a = self.read_rel_ptr(1);
-                let b = self.read_rel_ptr(2);
-                let c_idx = self.read_rel(3);
-                self.values[c_idx] = a * b;
-            },
-            other => panic!(format!("Processing unknown opcode: \"{}\"", other)),
-        }
-
-        self.program_counter += 4;
+        let instr = Instruction::fetch_and_decode(self);
+        instr.execute(self);
+        self.program_counter += instr.opcode.length();
     }
 
     fn run_to_completion(&mut self) {
-        while !self.terminated() {
+        while !self.terminated {
             self.progress_state();
         }
     }
@@ -110,15 +179,15 @@ fn main() {
     let file = File::open("./input.txt").expect("Failed to open input file");
     let reader = BufReader::new(file);
 
-    let values: Vec<usize> = reader
+    let values: Vec<_> = reader
         .split(b',')
         .map(|el| el.expect("Failed to read bytes from file"))
         .map(|el| String::from_utf8(el).expect("Bytes between a comma weren't UTF8"))
         .map(|el| el.trim().to_string())
-        .map(|el| el.parse::<usize>().expect(&format!("Failed to parse {} as u64", el)))
+        .map(|el| el.parse::<ProgramElement>().expect(&format!("Failed to parse {} as u64", el)))
         .collect();
 
-    let target = 19690720usize;
+    let target = 19690720;
 
     'outer: for noun in 0..100 {
         for verb in 0..100 {
