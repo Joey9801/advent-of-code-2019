6372d328b9503ba0119b649341a112f656049257
Joe Roberts <joe@jwjr.co.uk>
2020-01-31 09:19:04 +0000

    Day 9, part 2 problem statement + solution

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -2,7 +2,7 @@ use intcode_vm::ProgramState;
 
 fn main() {
     let mut program = ProgramState::load_program_file(std::path::Path::new("./input.txt"));
-    program.inputs.push_back(1);
+    program.inputs.push_back(2);
     program.run_to_completion();
     dbg!(&program.outputs);
 }
